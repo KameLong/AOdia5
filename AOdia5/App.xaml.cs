d6e5814dbb49164cbf0c99bb624d88e3e01caf07
unknown <kamelong>
2023-02-18 20:57:24 +0900

    VS2022でプロジェクトを作っただけ

---
+++ AOdia5/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AOdia5;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AOdia5/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AOdia5;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
